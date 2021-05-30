9f6a2b5f3d2d3ff5ef7c41f0e429585e53e0e40b
hgrranzi <vkh.mariia@gmail.com>
2021-05-30 10:02:38 +0300

    Created the header

---
+++ fil_di_ferro.h
@@ -0,0 +1,16 @@
+/* *********************************************** */
+/*                                                 */
+/*       by: hgrranzi <vkh.mariia@gmail.com>       */
+/*                                                 */
+/* *********************************************** */
+
+#ifndef FIL_DI_FERRO_H
+# define FIL_DI_FERRO_H
+
+#include <math.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "mlx/mlx.h"
+
+#endif
