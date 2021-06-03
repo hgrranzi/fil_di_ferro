1e4504ac59d5743a242dd7417f5ca9e1333598eb
hgrranzi <vkh.mariia@gmail.com>
2021-06-03 15:49:20 +0300

    Added the put_pxl function

--- fil_di_ferro.h
+++ fil_di_ferro.h
@@ -55,5 +55,6 @@ void	remove_data(t_data *data);
 void	finish_it(int errnum, t_data *data);
 
 int		render_image(t_data *data);
+void	put_pxl(t_data *data, int x, int y, int color);
 
 #endif
--- render_image.c
+++ render_image.c
@@ -6,6 +6,17 @@
 
 #include "fil_di_ferro.h"
 
+void	put_pxl(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x >= 0 && y >= 0)
+	{
+		dst = data->image->addr + (y * data->image->line + x * (data->image->bpp / 8));
+		*(unsigned int *)dst = color;
+	}
+}
+
 int	render_image(t_data *data)
 {
 	mlx_put_image_to_window(data->mlx_p, data->win_p, data->image, 0, 0);
